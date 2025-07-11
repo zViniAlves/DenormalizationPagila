52d17b97234c570d393eca3f4a94a0575db0c0e4
zViniAlves <alve.vini.silva@gmail.com>
2025-07-10 23:02:21 -0300

    feat: config pagila shell script

---
+++ config_pagila.sh
@@ -0,0 +1,39 @@
+# Import enviroment variables from .env
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+else
+  echo "File not found: .env"
+  exit 1
+fi
+
+# Installing and initialiazing postgres
+if command -v psql >/dev/null 2>&1 && sudo service postgresql status | grep -q "active (running)"; then
+  echo "Postgres alredy installed"
+else
+  sudo apt update -y
+  sudo apt install -y postgresql postgresql-contrib
+
+  sudo systemctl enable postgresql
+  sudo systemctl start postgresql
+fi 
+
+sudo -u postgres psql <<EOF
+CREATE USER $USER_DB WITH PASSWORD '$PSW_DB';
+CREATE DATABASE pagila OWNER $USER_DB;
+EOF
+
+# Downloanding Pagila files
+if [ -d "pagila" ]; then
+    cd pagila
+else
+    mkdir pagila && cd pagila
+fi
+
+wget https://github.com/devrimgunduz/pagila/raw/master/pagila-schema.sql
+wget https://github.com/devrimgunduz/pagila/raw/master/pagila-data.sql
+
+# Intalling Pagila
+sudo -u postgres psql -d pagila -f pagila-schema.sql
+sudo -u postgres psql -d pagila -f pagila-data.sql
+
+echo "Pagila instaled!"
